2e9945c9270f4fd861eb96acdb0c54c7c78b5d4c
mktheobro <theobrokuradoru@yahoo.co.jp>
2021-04-23 22:32:18 +0900

    編集ができるように修正

--- app/models/items_tag.rb
+++ app/models/items_tag.rb
@@ -19,16 +19,52 @@ class ItemsTag
     validates :image
   end
 
-  
 
-  def save
-    item = Item.create(name: name, info: info, category_id: category_id, sales_status_id: sales_status_id,
-                       shipping_fee_status_id: shipping_fee_status_id, prefecture_id: prefecture_id, scheduled_delivery_id: scheduled_delivery_id, price: price,user_id: user_id, image: image)
-    tag = Tag.where(tag_name: tag_name).first_or_initialize
-    tag.save
+  delegate :persisted?, to: :item
 
-    ItemTagRelation.create(item_id: item.id, tag_id: tag.id)
+  def initialize(attributes = nil, item: Item.new)
+    @item = item
+    attributes ||= default_attributes
+    super(attributes)
   end
 
-  
+
+  def save
+     return if invalid?
+     ActiveRecord::Base.transaction do
+      tags = split_tag_names.map { |tag_name| Tag.find_or_create_by!(tag_name: tag_name) }
+      item.update!(name: name, info: info, category_id: category_id, sales_status_id: sales_status_id,
+      shipping_fee_status_id: shipping_fee_status_id, prefecture_id: prefecture_id, scheduled_delivery_id: scheduled_delivery_id, price: price,user_id: user_id, image: image,tags: tags)
+     end
+  rescue ActiveRecord::RecordInvalid
+      false
+  end
+
+  def to_model
+    item
+  end
+
+  private
+
+  attr_reader :item
+
+  def default_attributes
+    {
+      name: item.name,
+      info: item.info,
+      category_id: item.category_id,
+      sales_status_id: item.sales_status_id,
+      shipping_fee_status_id: item.shipping_fee_status_id,
+      prefecture_id: item.prefecture_id,
+      scheduled_delivery_id: item.scheduled_delivery_id,
+      price: item.price,
+      user_id: item.user_id,
+      image: item.image,
+      tag_name: item.tags.pluck(:tag_name).join(',')
+    }
+  end
+
+  def split_tag_names
+    tag_name.split(',')
+  end
 end
