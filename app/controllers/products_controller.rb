c034100bac3cf04135a3b96492f40340e1b7b24e
mktheobro <theobrokuradoru@yahoo.co.jp>
2021-04-29 09:40:23 +0900

    検索機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,18 @@ class ProductsController < ApplicationController
   before_action :search_product, only: [:index, :search]
 
   def index
-    @items = Item.all
-    set_product_column
+    # @items = Item.all.order("created_at DESC")
+    # set_product_column
+    # set_tag_column
+    # @users = User.all
+    @tags = Tag.all
+    @items = @p.result.includes(:user, :tags)
   end
 
   def search
-    @results = @p.result.includes(:user)  # 検索条件にマッチした商品の情報を取得
-
+    # @results = @p.result.includes(search_params)  # 検索条件にマッチした商品の情報を取得
+    @p = Item.search(search_params)
+    @items = @p.result.includes(:user, :tags)
   end
 
   private
@@ -17,8 +22,12 @@ class ProductsController < ApplicationController
     @p = Item.ransack(params[:q])  # 検索オブジェクトを生成
   end
 
-  def set_product_column
-    @item_name = Item.select("name").distinct  # 重複なくnameカラムのデータを取り出す
+  def search_params
+     params.require(:q).permit(:name_cont, :price_lteq, :category_id_eq, :sales_status_id_eq, :shipping_fee_status_id_eq, :scheduled_delivery_id_eq, :tags_id_eq)
   end
+
+  # def set_tag_column
+    # @item_tags = ItemTagRelation.select("tag_id").distinct
+  # end
 
 end
