76b5fe7b6a685197bbf2bf4e834f6f45f6b85d63
mktheobro <theobrokuradoru@yahoo.co.jp>
2021-04-29 19:38:14 +0900

    売却済みの商品は検索結果に表示されないように修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,16 +2,11 @@ class ProductsController < ApplicationController
   before_action :search_product, only: [:index, :search]
 
   def index
-    # @items = Item.all.order("created_at DESC")
-    # set_product_column
-    # set_tag_column
-    # @users = User.all
     @tags = Tag.all
     @items = @p.result.includes(:user, :tags)
   end
 
   def search
-    # @results = @p.result.includes(search_params)  # 検索条件にマッチした商品の情報を取得
     @p = Item.search(search_params)
     @items = @p.result.includes(:user, :tags)
   end
@@ -25,9 +20,5 @@ class ProductsController < ApplicationController
   def search_params
      params.require(:q).permit(:name_cont, :price_lteq, :category_id_eq, :sales_status_id_eq, :shipping_fee_status_id_eq, :scheduled_delivery_id_eq, :tags_id_eq)
   end
-
-  # def set_tag_column
-    # @item_tags = ItemTagRelation.select("tag_id").distinct
-  # end
 
 end
